d778eb0f966fe97bab7880ac1186a39abbb450e3
Emiel Kwakkel <emiel.kwakkel@rabobank.nl>
2017-09-28 23:50:54 +0200

    Use shared location component.

--- src/pages/location/location.module.ts
+++ src/pages/location/location.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from "@angular/core";
-import { LocationPage } from "./location";
-import { Geolocation } from "@ionic-native/geolocation";
-import { IonicPageModule } from "ionic-angular";
+import { NgModule } from '@angular/core';
+import { LocationPage } from './location';
+import { Geolocation } from '@ionic-native/geolocation';
+import { IonicPageModule } from 'ionic-angular';
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
     declarations: [LocationPage],
-    imports: [IonicPageModule.forChild(LocationPage)],
+    imports: [IonicPageModule.forChild(LocationPage), SharedModule],
     entryComponents: [LocationPage],
     providers: [Geolocation]
 })

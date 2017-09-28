d8c4cde7d0b97b0629faf8f4a99387308e46cffd
Emiel Kwakkel <emiel.kwakkel@rabobank.nl>
2017-09-28 23:49:59 +0200

    Import shared module

--- src/pages/company/add/company-add.module.ts
+++ src/pages/company/add/company-add.module.ts
@@ -3,10 +3,15 @@ import { IonicPageModule } from "ionic-angular";
 import { LocationPageModule } from "../../location/location.module";
 import { CompanyAddPage } from "./company-add";
 import { CompanyService } from "../company.service";
+import { SharedModule } from '../../../shared/shared.module';
 
 @NgModule({
   declarations: [CompanyAddPage],
-  imports: [IonicPageModule.forChild(CompanyAddPage), LocationPageModule],
+  imports: [
+    IonicPageModule.forChild(CompanyAddPage),
+    LocationPageModule,
+    SharedModule
+  ],
   entryComponents: [CompanyAddPage],
   providers: [CompanyService]
 })

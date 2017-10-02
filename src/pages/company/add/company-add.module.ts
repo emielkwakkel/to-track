91747a22b8797e7d46b745223df2c513eca003c3
Emiel Kwakkel <emiel.kwakkel@rabobank.nl>
2017-10-02 20:49:48 +0200

    Remove unused module.

--- src/pages/company/add/company-add.module.ts
+++ src/pages/company/add/company-add.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from "@angular/core";
 import { IonicPageModule } from "ionic-angular";
-import { LocationPageModule } from "../../location/location.module";
 import { CompanyAddPage } from "./company-add";
 import { CompanyService } from "../company.service";
 import { SharedModule } from '../../../shared/shared.module';
 
 @NgModule({
   declarations: [CompanyAddPage],
-  imports: [
-    IonicPageModule.forChild(CompanyAddPage),
-    LocationPageModule,
-    SharedModule
-  ],
+  imports: [ IonicPageModule.forChild(CompanyAddPage), SharedModule ],
   entryComponents: [CompanyAddPage],
   providers: [CompanyService]
 })

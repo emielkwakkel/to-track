91bad080deb849f5f4765ec35fd39dc0721f197a
Emiel Kwakkel <emiel.kwakkel@rabobank.nl>
2017-10-07 17:34:37 +0200

    Get user driver information.

--- src/user/user.ts
+++ src/user/user.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 import * as firebase from 'firebase/app';
 
 import { AuthenticationService } from '../shared/user/authentication.service';
@@ -12,15 +13,18 @@ import { UserService } from '../shared/user/user.service';
 })
 export class UserPage implements OnInit {
     user: firebase.User;
+    driver: string = null;
 
     constructor(
         public navCtrl: NavController,
         private authenticationService: AuthenticationService,
-        private userService: UserService) {
+        private userService: UserService,
+        private storage: Storage) {
     }
 
     ngOnInit() {
       this.user = this.userService.user;
+      this.driver = this.storage.driver;
     }
 
     public logout() {

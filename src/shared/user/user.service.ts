11bec6e278027258b73a4aa5cc38f7e527efcaac
Emiel Kwakkel <emiel.kwakkel@rabobank.nl>
2017-10-07 17:33:53 +0200

    Make user service more robust. Stores user in service and local storage. If user can't be found in either of them it will be loaded from firebase.

--- src/shared/user/user.service.ts
+++ src/shared/user/user.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 import * as firebase from 'firebase/app';
 
 @Injectable()
@@ -6,26 +7,50 @@ export class UserService {
     private _user: firebase.User;
     private _database = firebase.database;
 
-    constructor() {}
+    constructor(private storage: Storage) {
+    }
 
     get user(): firebase.User {
+        console.log('get user', this._user)
+        if(!this._user) {
+          console.log('User not locally stored');
+          this.storage.get('user')
+            .then(user => {
+              console.log('result', user);
+              if (user) this.user = user;
+              if (!user) this.user = firebase.auth().currentUser;
+            })
+            .catch(error => {
+              console.log('error getting user', error)
+            });
+        } else {
+          console.log('user not logged in');
+        }
         return this._user;
     }
 
     set user(user: firebase.User) {
+        console.log('Set user', user);
+
         if (user) {
+          // Store between page loads
           this._user = user;
-          console.log('Set user', user);
-  				this.writeUserData(
-  					user.uid,
-  					user.displayName,
-  					user.email,
-  					user.photoURL,
-  					user.providerData[0].providerId,
-  					user.phoneNumber
-  				).then(() => {
-  					console.log('Written user data');
-  				}).catch(error => {
+
+          // Store on device
+          this.storage.remove('user');
+          this.storage.set('user', user);
+
+          // Write to storage
+          this.writeUserData(
+            user.uid,
+            user.displayName,
+            user.email,
+            user.photoURL,
+            user.providerData[0].providerId,
+            user.phoneNumber
+          ).then(() => {
+            console.log('Written user data');
+          }).catch(error => {
             console.log('Error writing user data');
           })
         } else {
@@ -34,12 +59,14 @@ export class UserService {
     }
 
     writeUserData(uid : string, displayName: string, email : string, photoURL : string, provider: string, phoneNumber : string) {
-        return this._database().ref('users/' + uid).set({
-            displayName: displayName,
-            email: email,
-            photoURL: photoURL,
-						provider: provider,
-						phoneNumber: phoneNumber
-        });
+        return this._database()
+            .ref('users/' + uid)
+            .set({
+                displayName,
+                email,
+                photoURL,
+                provider,
+                phoneNumber
+            });
     }
 }

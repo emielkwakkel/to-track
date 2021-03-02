3e5c77f32dc1e4f102a8deecf6013f84ff239024
Emiel Kwakkel <mail@emielkwakkel.nl>
2021-03-02 23:39:06 +0100

    Hide secrets from plain-sight and delete current api key

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,11 +1,13 @@
+import { secrets } from './secrets';
+
 /**
  * @type {Object} Config for Firebase.
  */
 export const firebaseConfig = {
-    apiKey: "AIzaSyAzyiVHhDaTG3Coy0oyJc0UF7Iqtu188VM",
-    authDomain: "totrack-42d42.firebaseapp.com",
-    databaseURL: "https://totrack-42d42.firebaseio.com",
-    projectId: "totrack-42d42",
-    storageBucket: "totrack-42d42.appspot.com",
-    messagingSenderId: "1004590595067"
+    apiKey: secrets.apiKey,
+    authDomain: secrets.authDomain,
+    databaseURL: secrets.databaseURL,
+    projectId: secrets.projectId,
+    storageBucket: secrets.storageBucket,
+    messagingSenderId: secrets.messagingSenderId,
 };
